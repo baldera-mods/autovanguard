ccc1a264d5cdea23aa6f05c0b31650b802c021e7
ayylmar <34417135+ayylmar@users.noreply.github.com>
2017-12-26 03:01:01 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function AutoVanguard(dispatch) {
-  dispatch.hook('sCompleteEventMatchingQuest', function(event) {
-    dispatch.toServer('cCompleteDailyEvent', { id: event.id });
-    return false;
-  });
-};
